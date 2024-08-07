1bd5de19d78aca97fed2a37f69a111f5c12e06f8
KatoMasami <163547645+katomasami@users.noreply.github.com>
2024-08-07 13:11:32 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:new, :create, :show, :edit, :update, :index]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
+  # devise_scope :user do
+  #   get '/users/sign_out', to: 'devise/sessions#destroy'
+  # end
+  root to: "homes#top"
+  get '/home/about' => 'homes#about' ,as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
